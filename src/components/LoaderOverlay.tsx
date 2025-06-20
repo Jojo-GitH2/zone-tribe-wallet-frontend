67de162b3e157efd6aad44dd383f975d5bed6bb8
Jonah Uka <jonah.uka@gmail.com>
2025-06-20 12:48:35 +0100

    Remove unused Box import from LoaderOverlay component

--- src/components/LoaderOverlay.tsx
+++ src/components/LoaderOverlay.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, CircularProgress, Typography, Paper } from "@mui/material";
+import { CircularProgress, Typography, Paper } from "@mui/material";
 
 const LoaderOverlay: React.FC<{ message?: string }> = ({ message = "Loading..." }) => (
   <Paper
